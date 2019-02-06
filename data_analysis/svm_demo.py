5c4755d91d737632e866ee72079a75f592012e29
epicarts <0505zxc@gmail.com>
2019-02-06 23:42:50 +0900

    upload

--- data_analysis/svm_demo.py
+++ data_analysis/svm_demo.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_circles
 from sklearn import svm, metrics
+%matplotlib inline
 
+'''
+https://www.kaggle.com/maxl11/svm-demo
+'''
 
 def make_meshgrid(x, y, h=.02):
     """Create a mesh of points to plot in
@@ -47,8 +51,40 @@ train_prop = 5.0
 x,y = make_circles(n_samples=samples, noise=0.05, random_state=123)
 x[:,0]
 x[:,0]
-df = pd.DataFrame({x=x[:, 0], y=x[:, 1]}, label=y))
-
+df = pd.DataFrame(dict(x=x[:, 0], y=x[:, 1], label=y))
+df
 groups = df.groupby('label')
 groups
-ax.margins adfa
+
+fig, ax = plt.subplots()
+ax.margins(0.05)  # Optional, just adds 5% padding to the autoscaling
+for name, group in groups:#그룹별로 분해.
+    ax.plot(group.x, group.y, marker='o', linestyle='', ms=6, label=name)
+ax.legend()
+plt.show()
+
+x.min()
+x.max()
+
+# 0 ~ 1 사이로 스케일링 함.
+x = (x-x.min())/(x.max()-x.min())
+x.min()
+x.max()
+x.mean()
+
+
+C = 1.0  # SVM 정규화 파라미터
+models = svm.SVC(kernel='linear', C=C)
+models.fit(x, y)
+
+'''
+정수를 담은 이차원 리스트, mylist 가 solution 함수의 파라미터로 주어집니다.
+ solution 함수가 mylist 각 원소의 길이를 담은 리스트를 리턴하도록 코드를
+작성해주세요.
+
+input [[1], [2]] /[[1, 2], [3, 4], [5]]
+
+output
+ [1,1]
+ [2,2,1]
+'''
